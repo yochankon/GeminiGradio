537d9c690756eef7299967305569ac0708b4ee2d
공요찬 <pj.yochan.kong@kalmate.net>
2024-07-11 16:35:49 +0900

    TEST gradio2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app_test4 import get_gemini_response
+import uvicorn
 
 app = FastAPI()
 
@@ -7,3 +8,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, port=9500, host='0.0.0.0', reload= True)
