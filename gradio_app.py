2091a0828ae0df6c89990d9ea0cc4866afd35c75
공요찬 <pj.yochan.kong@kalmate.net>
2024-07-11 20:39:38 +0900

    TEST gradio5

--- gradio_app.py
+++ gradio_app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import gradio as gr
 import google.generativeai as genai
 
+
 app = FastAPI()
 
 GOOGLE_API_KEY = 'AIzaSyBmccPBmZavxweXuiRI1E4tICHsJzFMX9I'
@@ -19,17 +20,17 @@ genai.configure(api_key=GOOGLE_API_KEY)
 # # Then run `uvicorn run:app` from the terminal and navigate to http://localhost:8000/gradio.
 #
 #
-# prompt = [
-#     """
-#     You are an expert in converting English question to SQL query!
-#     The SQL database has the name STUDENT ans has the following columns - NAME, CLASS, SECTION and MARKS \n\n
-#     For Example, \nExample 1 - How many entries of records are present in the SQL command will be something this
-#     SELECT COUNT(*) FROM STUDENT ;
-#     \nExample 2 - Tell me all the students studying  in Data Science class?,
-#     the SQL command will be something like this SELECT * FROM STUDENT where CLASS = "DATA Science";
-#     also the sql code should not have this ``` in beginning or at the end, sql word should not be in output and Just give sql command for the given question.
-# """
-# ]
+prompt = [
+    """
+    You are an expert in converting English question to SQL query!
+    The SQL database has the name STUDENT ans has the following columns - NAME, CLASS, SECTION and MARKS \n\n
+    For Example, \nExample 1 - How many entries of records are present in the SQL command will be something this
+    SELECT COUNT(*) FROM STUDENT ;
+    \nExample 2 - Tell me all the students studying  in Data Science class?,
+    the SQL command will be something like this SELECT * FROM STUDENT where CLASS = "DATA Science";
+    also the sql code should not have this ``` in beginning or at the end, sql word should not be in output and Just give sql command for the given question.
+"""
+]
 
 @app.get("/")
 def get_gemini_response(text, prompt):
